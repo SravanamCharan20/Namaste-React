3533f8882a16e02894e287cc8fc5422e0e47e0d4
charan <sravanamcharan20@gmail.com>
2026-01-22 16:17:25 +0530

    Inner working of hello React

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
-const header = document.createElement("h1");
-header.innerHTML = "Hello from JS";
-console.log(header);
-const root = document.getElementById("root");
-root.appendChild(header);
+// const header = document.createElement("h1");
+// header.innerHTML = "Hello from JS";
+// console.log(header);
+// const root = document.getElementById("root");
+// root.appendChild(header);
+
+const header = React.createElement(
+  "h1",
+  { id: "header", charan: "Meghana" },
+  "this is from H1 tag from React"
+);
+console.log(header); // return object contains this 👇
+// {
+// props:
+//     charan: "Meghana"
+//     children: "this is from H1 tag from React"
+//     id: "header"
+// type: "h1"
+// }
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(header);
+// render takes that object and creates the tag ans inject the children info into the tag.
